2a395cfbd5323a9a523ac6485cc666ead2eb8516
andrekruss <30608106+andrekruss@users.noreply.github.com>
2021-02-08 22:26:25 -0300

    First example program

---
+++ Exemplos/HelloWorld/HelloWorld.java
@@ -0,0 +1,9 @@
+// This is a simple Java program that prints Hello World!
+// Compile program using "javac HelloWorld.java"
+// Run bytecode using "java HelloWorld"
+class HelloWorld{
+
+    public static void main(String[] args){
+        System.out.println("Hello World!");
+    }
+}
